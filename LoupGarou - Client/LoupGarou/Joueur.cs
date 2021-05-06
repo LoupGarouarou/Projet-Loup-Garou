c30cb9a0cb4963e0b762cbc6cab0c571ebabe756
Pouuiou <83730066+Pouuiou@users.noreply.github.com>
2021-05-06 14:09:30 +0200

    Add files via upload

---
+++ LoupGarou - Client/LoupGarou/Joueur.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoupGarou
+{
+    public class Joueur : Client
+    {
+        public bool estActif;
+        public bool estMort;
+        public string leLogin;
+        public int leNbreVote;
+        public Role leRole;
+
+        public Joueur()
+        {
+
+        }
+    }
+}
---
+++ LoupGarou - Client/LoupGarou/Jour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LoupGarou
+{
+    public class Jour
+    {
+        int auto;
+        int lesMorts;
+        bool lEtat;
+        int lid;
+
+        public Jour()
+        {
+
+        }
+    }
+}
