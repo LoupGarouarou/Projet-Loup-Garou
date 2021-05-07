4e4967115c2cb8b83f83b61ea19614aa8fc11b47
btssio20192020 <mmelerayd@gmail.com>
2021-05-07 22:01:40 +0200

    Update Joueur.cs
    Modification à apporter à la classe Joueur

--- LoupGarou - Client/LoupGarou/Joueur.cs
+++ LoupGarou - Client/LoupGarou/Joueur.cs
@@ -18,9 +18,23 @@ namespace LoupGarou
         public int leNbreVote;
         public Role leRole;
 
-        public Joueur()
+        //Constructeur pour le test en mode console
+        public Joueur(string login, int num) : base(num)
         {
+            this.leLogin = login;
+        }
+        //Constructeur pour le test avec la création du joueur provenant du serveur.
+        public Joueur(string login, string lAdresseS, string lAdresseC, int num, TextReader leTR, TextWriter leTW) : base(lAdresseS, lAdresseC, num, leTR, leTW)
+        {
+            this.numero = num;
+            this.leLogin = login;
+        }
 
+        //Constructeur pour la création du joueur provenant du client.
+        public Joueur(string login, string lAdresseS, int lePortS, string lAdresseC, int num, Plateforme laPlateforme) : base(lAdresseS, lePortS, lAdresseC, num, laPlateforme)
+        {
+            this.numero = num;
+            this.leLogin = login;
         }
     }
 }
